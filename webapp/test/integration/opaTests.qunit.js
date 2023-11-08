05e4e17841b92c64e923ee96ef9f65ccd327f78c
Sunil Singh A <sunil.singh.a01@sap.com>
2023-11-08 15:10:41 +0000

    init

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["camerapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"camerapp/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
